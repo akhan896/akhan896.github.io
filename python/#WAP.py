bf12c7e6276ce99db1321ec96553b71453bf7f26
Your Name <you@example.com>
2023-10-19 16:15:14 +0530

    abc

--- python/#WAP.py
+++
@@ -1,8 +0,0 @@
-#WAP to find the sum of to numbers 
-print("Enter first number: ")
-num1 = int(input())
-print("Enter second number: ")
-num2 = int(input())
-
-sum = num1 + num2
-print("sum : ",sum)
